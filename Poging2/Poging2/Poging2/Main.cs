c42d77601e378251f37a3bbad02c8044ce92bb63
markvenneker <markvenneker.mv@gmail.com>
2017-09-05 13:46:57 +0200

    test

---
+++ Poging2/Poging2/Poging2/Main.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using MonoBrickFirmware;
+using MonoBrickFirmware.Display.Dialogs;
+using MonoBrickFirmware.Display;
+using MonoBrickFirmware.Movement;
+using System.Threading;
+
+namespace MonoBrickHelloWorld
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Motor motor = new Motor (MotorPort.OutA);
+			motor.SetSpeed (50);
+			Thread.Sleep (3000);
+			motor.Off ();
+			Lcd.Clear ();
+			Lcd.Update ();
+			// hello
+		}
+	}
+}
+
