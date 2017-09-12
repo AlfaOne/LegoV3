63c581fb967c3feb10a7cb15732b62a9208f53b2
markvenneker <markvenneker.mv@gmail.com>
2017-09-12 15:42:10 +0200

    wijzigingen

--- Poging2/Poging2/Poging2/Main.cs
+++ Poging2/Poging2/Poging2/Main.cs
@@ -4,53 +4,92 @@ using MonoBrickFirmware.Sensors;
 using MonoBrickFirmware.Display.Dialogs;
 using MonoBrickFirmware.Display;
 using MonoBrickFirmware.Movement;
+using MonoBrickFirmware.UserInput;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace MonoBrickHelloWorld
 {
 	class MainClass
 	{
-		public static void Main (string[] args)
+		public static void Hefarm (sbyte i, int j)
+		{
+			Motor motorC = new Motor (MotorPort.OutC);
+			motorC.SetSpeed (i);
+			Thread.Sleep (j);
+			motorC.Off ();
+		}
+
+		public static void Motoren (sbyte i, int j, sbyte k)
 		{
 			Motor motor = new Motor (MotorPort.OutA);
-//			motor.SetSpeed (50);
-//			Thread.Sleep (3000);
-//			motor.Off ();
-//			Lcd.Clear ();
-//			Lcd.Update ();
-//
-			//Motor Poort B
 			Motor motorB = new Motor (MotorPort.OutB);
-//			motorB.SetSpeed (50);
-//			Thread.Sleep (3000);
-//			motorB.Off ();
-//			Lcd.Clear ();
-//			Lcd.Update ();
-		
-			//Hefarm
-			Motor motorC = new Motor (MotorPort.OutC);
-			motorC.SetSpeed (20);
-			Thread.Sleep (1000);
-			motorC.SetSpeed (-20);
-			Thread.Sleep (1000);
-			motorC.Off ();
-
-			//UltraSonic Sensor
-			MonoBrickFirmware.Sensors.EV3UltrasonicSensor UltraSensor = new MonoBrickFirmware.Sensors.EV3UltrasonicSensor (MonoBrickFirmware.Sensors.SensorPort.In1);
-			int distance = UltraSensor.Read ();
-			Console.WriteLine ("Distance " + distance);
-
-
-			motor.SetSpeed (100);
-			motorB.SetSpeed (-100);
-			Thread.Sleep (4000);
+			motor.SetSpeed (i);
+			motorB.SetSpeed (k);
+			Thread.Sleep (j);
 			motor.Off ();
 			motorB.Off ();
-
-			if (distance < 30) {
-				motor.Off ();
-				motorB.Off ();
+		}
+			
+		public static int SensorAfstand() 
+		{
+			MonoBrickFirmware.Sensors.EV3UltrasonicSensor UltraSensor = new MonoBrickFirmware.Sensors.EV3UltrasonicSensor (MonoBrickFirmware.Sensors.SensorPort.In1);
+			int distance = UltraSensor.Read ();
+			LcdConsole.WriteLine ("Distance: "+ distance);
+			return distance;
 			}
+
+		public static void Main (string[] args)
+		{
+//			int n = 1;
+//			while (n < 6) {
+//				if ( SensorAfstand() < 100) {
+//					Hefarm (-10, 1000);
+//				}
+//				if ( SensorAfstand() > 100) {
+//					Hefarm (10, 1000);
+//				}
+//				LcdConsole.WriteLine ("Distance: " + SensorAfstand());
+//			}
+
+			int n = 1;
+						while (n < 6) {
+							if ( SensorAfstand() < 100) {
+					Motoren (50, 1000, 50);
+							}
+							if ( SensorAfstand() > 100) {
+					Motoren (-50, 1000, -50);
+				}
+					Hefarm (20, 500);
+					Hefarm (-20, 500);
+							LcdConsole.WriteLine ("Distance: " + SensorAfstand());
+						}
+
+			//Hefarm
+			for (int i = 0; i < 10; i++) {
+				Hefarm (20, 500);
+				Hefarm (-20, 500);
+				if (i == 4) {
+					break;
+				};
+				}
+
+//				for (int x=0; x<1; x++) {
+//					TastenStatus = Buttons.GetKeypress();
+//					if (TastenStatus == Buttons.ButtonStates.Enter) { terminateProgram.Set();}
+//				}
+//
+//
+//			motor.SetSpeed (100);
+//			motorB.SetSpeed (-100);
+//			Thread.Sleep (4000);
+//			motor.Off ();
+//			motorB.Off ();
+//
+//			if (distance < 30) {
+//				motor.Off ();
+//				motorB.Off ();
+//			}
 
 //			if (distance >100) {
 //				Console.WriteLine ("boven 100");
@@ -70,5 +109,5 @@ namespace MonoBrickHelloWorld
 
 		}
 	}
-}
+	}
 
